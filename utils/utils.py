a44185617fe1dcc6dc98f170e6a724e6003c77e0
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2024-02-13 17:09:35 -0500

    fixed something time for tests

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,8 @@
 import re
 
 
-def findTempAndDegree(message: str) -> tuple[int, str] | None:
+def findTempAndDegree(message: str) -> tuple[float, str] | None:
     v = re.search(r'-?(\d+|\d*[.,]\d+|\d+.\d*)\s*°?\s*(C|F|c|f)\b', message)
     if v == None:
         return None
-    return (int(v.groups()[0]), v.groups()[1])
+    return (float(v.groups()[0]), v.groups()[1])
