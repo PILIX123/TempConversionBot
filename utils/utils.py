9589cd671742545772de00e5279d09dca91937b1
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2024-02-13 20:11:59 -0500

    fixed tests

--- utils/utils.py
+++ utils/utils.py
@@ -5,5 +5,4 @@ def findTempAndDegree(message: str) -> list[tuple[float, str]] | None:
     v = re.findall(r'(-?\d+|\d*[.,]\d+|\d+.\d*)\s*°?\s*(C|F|c|f)\b', message)
     if v == []:
         return None
-    t = [(float(va[0]), va[1]) for va in v]
     return [(float(va[0]), va[1]) for va in v]
